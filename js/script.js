a7cdfe3fdb03cd3ae00af817e0705489866583d1
otaviools <otaviools13@gmail.com>
2026-02-09 10:36:13 -0300

    otimizacao

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
